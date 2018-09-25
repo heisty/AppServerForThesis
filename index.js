a20c96936646f2a116a3d33708b13cbdcff0a065
heisty <thymyriadcolors@gmail.com>
2018-09-25 11:11:34 +0800

    j

--- index.js
+++ index.js
@@ -7,30 +7,30 @@ const cors = require('cors');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/jnlbha');
+// mongoose.connect('mongodb://localhost/jnlbha');
 
 
-
-app.use(morgan('combined'));
-app.use(cors());
-app.use(bodyParser.json());
-app.use('/v1',router);
-
-var PORT = process.env.PORT || 3000;
-var HOST = process.env.HOST || '192.168.30.3';
-
-console.log('Listening on ',PORT);
-app.listen(PORT,HOST);
-
-// ON SERVER
-
-// mongoose.connect('mongodb://jnladmin:jnlpassword123@ds131312.mlab.com:31312/jnlbah');
 
 // app.use(morgan('combined'));
+// app.use(cors());
 // app.use(bodyParser.json());
 // app.use('/v1',router);
 
 // var PORT = process.env.PORT || 3000;
+// var HOST = process.env.HOST || '192.168.30.3';
 
 // console.log('Listening on ',PORT);
-// app.listen(PORT);  
+// app.listen(PORT,HOST);
+
+//ON SERVER
+
+mongoose.connect('mongodb://admin:123admin@ds045087.mlab.com:45087/jnlbha');
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use('/v1',router);
+
+var PORT = process.env.PORT || 3000;
+
+console.log('Listening on ',PORT);
+app.listen(PORT);  
